bf6dca782435a1768252b8503af516b4056ec5cd
MonsterGuo <39860733+MonsterGuo@users.noreply.github.com>
2023-05-16 23:41:44 +0800

    提交5.2版本

---
+++ Ansel5.2/Ansel/Intermediate/Build/Win64/UnrealEditor/Inc/Ansel/UHT/AnselCapture.generated.h
@@ -0,0 +1,110 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "AnselCapture.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ANSEL_AnselCapture_generated_h
+#error "AnselCapture.generated.h already included, missing '#pragma once' in AnselCapture.h"
+#endif
+#define ANSEL_AnselCapture_generated_h
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS \
+	virtual void CallSequncePlayer_Implementation(); \
+ \
+	DECLARE_FUNCTION(execForceQualitySettings); \
+	DECLARE_FUNCTION(execCallSequncePlayer);
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execForceQualitySettings); \
+	DECLARE_FUNCTION(execCallSequncePlayer);
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ACCESSORS
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAnselCapture(); \
+	friend struct Z_Construct_UClass_AAnselCapture_Statics; \
+public: \
+	DECLARE_CLASS(AAnselCapture, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Ansel"), NO_API) \
+	DECLARE_SERIALIZER(AAnselCapture)
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS \
+private: \
+	static void StaticRegisterNativesAAnselCapture(); \
+	friend struct Z_Construct_UClass_AAnselCapture_Statics; \
+public: \
+	DECLARE_CLASS(AAnselCapture, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Ansel"), NO_API) \
+	DECLARE_SERIALIZER(AAnselCapture)
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAnselCapture(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAnselCapture) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAnselCapture); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAnselCapture); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAnselCapture(AAnselCapture&&); \
+	NO_API AAnselCapture(const AAnselCapture&); \
+public: \
+	NO_API virtual ~AAnselCapture();
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAnselCapture(AAnselCapture&&); \
+	NO_API AAnselCapture(const AAnselCapture&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAnselCapture); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAnselCapture); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AAnselCapture) \
+	NO_API virtual ~AAnselCapture();
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_7_PROLOG
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ACCESSORS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_SPARSE_DATA \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ACCESSORS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_CALLBACK_WRAPPERS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_INCLASS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ANSEL_API UClass* StaticClass<class AAnselCapture>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_Ansel_Source_Ansel_Public_AnselCapture_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
